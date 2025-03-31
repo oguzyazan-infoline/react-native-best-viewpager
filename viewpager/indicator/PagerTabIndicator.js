aa5599b7fc2269d997f004c4b10fc6521fc9ea3c
Oğuz Kaan Yazan <89526970+oguzyazan-infoline@users.noreply.github.com>
2025-04-01 00:21:34 +0300

    Update PagerTabIndicator.js

--- viewpager/indicator/PagerTabIndicator.js
+++ viewpager/indicator/PagerTabIndicator.js
@@ -15,15 +15,15 @@ export default class PagerTabIndicator extends Component {
         pager: PropTypes.instanceOf(IndicatorViewPager),
         tabs: PropTypes.arrayOf(PropTypes.shape({
             text: PropTypes.string,
-            iconSource: Image.propTypes.source,
-            selectedIconSource: Image.propTypes.source
+            iconSource: Image.propTypes?.source,
+            selectedIconSource: Image.propTypes?.source
         })).isRequired,
         itemStyle: ViewPropTypes.style,
         selectedItemStyle: ViewPropTypes.style,
-        iconStyle: Image.propTypes.style,
-        selectedIconStyle: Image.propTypes.style,
-        textStyle: Text.propTypes.style,
-        selectedTextStyle: Text.propTypes.style,
+        iconStyle: Image.propTypes?.style,
+        selectedIconStyle: Image.propTypes?.style,
+        textStyle: Text.propTypes?.style,
+        selectedTextStyle: Text.propTypes?.style,
         changePageWithAnimation: PropTypes.bool,
     }
 
